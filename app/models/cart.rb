7ff74c20d5c42cc222178b50268ef499e8356e41
Kanwarpreet Sethi <kpsethi@gmail.com>
2012-01-25 02:07:31 -0500

    Cart AddProduct Bug

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,7 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy
   
-  def add_product(product_id, product_price)
+  def add_product(product_id)
     current_item = line_items.find_by_product_id(product_id)
     
     if current_item
