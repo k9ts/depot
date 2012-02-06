fedc9c6d8772bc2676e5b2a285735fcd96f8fa4b
Kanwarpreet Sethi <kpsethi@gmail.com>
2012-02-06 00:54:15 -0500

    After AJAX playtime

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,13 +1,6 @@
 class StoreController < ApplicationController
   
   def index
-    if session[:counter].nil?
-      counter = 1
-      session[:counter] = counter
-    else
-      session[:counter] += 1
-    end
-    
     @products = Product.all
     @cart = current_cart
   end
